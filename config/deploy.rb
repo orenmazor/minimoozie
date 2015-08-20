90976917ca29a76ec05a4a6d92f270e5824ed55f
oren mazor <oren.mazor@gmail.com>
2015-08-20 14:59:23 -0400

    rebuild image after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,18 @@ set :keep_releases, 2
 set :ejson_file, "config.ejson"
 set :ejson_output_file, "config.json"
 
+namespace :docker do
+  task :rebuild_image do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "cd #{release_path}; docker build -t minimoozie ."
+    end
+  end
+end
+
 namespace :deploy do
   after :restart, :clear_cache do
     execute "docker build -t minimoozie ."
   end
 end
+
+after :deploy, "docker:rebuild_image"
