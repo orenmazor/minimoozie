aa9a1898c6260de48098d055db31b2d64f485d0a
oren mazor <oren.mazor@gmail.com>
2015-08-20 20:49:33 -0400

    make sure to restart the container

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,10 +7,15 @@ set :keep_releases, 2
 set :ejson_file, "config.ejson"
 set :ejson_output_file, "config.json"
 
+
 namespace :deploy do
-  task :rebuild_image do
+  task :restart_docker_image do
     on roles(:app), in: :sequence, wait: 5 do
+      execute "docker stop minimoozie || true"
+      execute "docker rm minimoozie || true"
       execute "cd #{release_path}; docker build -t minimoozie ."
+      execute "docker run --net=host -d --name minimoozie minimoozie"
     end
   end
+  after :finished, :restart_docker_image
 end
